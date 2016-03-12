b14607bae1c746330e63cd1b9d51b3b08cc4a245
Emily Chao <elchao96@live.com>
2016-03-12 10:52:52 -0600

    started basis of visualization

---
+++ script.js
@@ -0,0 +1,9 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: {
+            lat: 40.106483,
+            lng: -88.2229657
+        },
+    });
+}

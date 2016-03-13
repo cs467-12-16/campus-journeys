c03edf5969d01beb62f7a28441fd2d3133174f98
Emily Chao <elchao96@live.com>
2016-03-12 18:16:17 -0600

    added data

--- script.js
+++ script.js
@@ -14,6 +14,31 @@ function showDay(value) {
     $('#currentDay').text(daysOfTheWeek[value-1]);
 }
 
+function parseData(data) {
+    $('#schedule').empty();
+    for(var y in data) {
+        if (y == $('#year').val()) {
+            for (var s in data[y]) {
+                if (s == $('#semester').val()) {
+                    var classes = data[y][s];
+                    for (var i = 0; i < classes.length; i++) {
+                        c = classes[i];
+                        console.log(c);
+                        $('#schedule').append('<li class="list-group-item">' + c.section + ' ' + c.number + '</li>');
+                    }
+                }
+            }
+        }
+    }
+}
+
+function getSuggested() {
+    $.getJSON('data/suggested/' + $('#major').val() + '_suggested.json', function(data) {
+        parseData(data);
+    });
+}
+
 $(document).ready(function() {
     showDay(3);
+    getSuggested();
 });

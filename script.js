260cca2f8e38882c815db45799437d1949d52c12
Emily Chao <elchao96@live.com>
2016-03-13 14:36:27 -0500

    done for now?

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+var map;
+
+var rectangles = {};
+
 function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(document.getElementById('map'), {
         zoom: 15,
         center: {
             lat: 40.106483,
@@ -8,14 +12,20 @@ function initMap() {
     });
 }
 
-var daysOfTheWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+var daysOfTheWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+var days = ['M', 'T', 'W', 'R', 'F'];
 
 function showDay(value) {
     $('#currentDay').text(daysOfTheWeek[value-1]);
+    getSuggested();
 }
 
 function parseData(data) {
     $('#schedule').empty();
+    var places = {};
+    for(var p in rectangles) {
+        places[p] = 0;
+    }
     for(var y in data) {
         if (y == $('#year').val()) {
             for (var s in data[y]) {
@@ -23,8 +33,21 @@ function parseData(data) {
                     var classes = data[y][s];
                     for (var i = 0; i < classes.length; i++) {
                         c = classes[i];
-                        console.log(c);
                         $('#schedule').append('<li class="list-group-item">' + c.section + ' ' + c.number + '</li>');
+                        var day = days[$('#day').val() - 1];
+                        if (c.locations[day] !== undefined) {
+                            for (var cl in c.locations[day]) {
+                                var place = c.locations[day][cl].place;
+                                if (place !== undefined)
+                                    places[place] += 1 / c.locations[day].length;
+                            }
+                        }
+                        console.log(places);
+                    }
+                    for (var t in places) {
+                        rectangles[t].setOptions({
+                            fillOpacity: places[t]
+                        });
                     }
                 }
             }
@@ -33,12 +56,34 @@ function parseData(data) {
 }
 
 function getSuggested() {
-    $.getJSON('data/suggested/' + $('#major').val() + '_suggested.json', function(data) {
+    $.getJSON('data/weekly/' + $('#major').val() + '_weekly.json', function(data) {
         parseData(data);
     });
 }
 
+function initializeRectangles() {
+    $.getJSON('data/locations.json', function(data) {
+        for(var classroom in data) {
+            rectangles[classroom] = new google.maps.Rectangle({
+                strokeColor: '#0000FF',
+                strokeOpacity: 0,
+                strokeWeight: 0,
+                fillColor: '#0000FF',
+                fillOpacity: 0,
+                map: map,
+                bounds: {
+                    north: data[classroom][0],
+                    south: data[classroom][2],
+                    east: data[classroom][1],
+                    west: data[classroom][3]
+                }
+            });
+        }
+    });
+}
+
 $(document).ready(function() {
-    showDay(3);
+    showDay(1);
+    initializeRectangles();
     getSuggested();
 });
